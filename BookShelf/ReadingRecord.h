fc1a2ced6e0ccd45948c400f49f64cc182901947
Brian Hu <billy170916@gmail.com>
2014-07-08 10:10:36 +0800

    structure of ReadingRecordTableView is ready

---
+++ BookShelf/ReadingRecord.h
@@ -0,0 +1,22 @@
+//
+//  ReadingRecord.h
+//  BookShelf
+//
+//  Created by Brian Hu on 7/8/14.
+//  Copyright (c) 2014 Brian Hu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Book;
+
+@interface ReadingRecord : NSManagedObject
+
+@property (nonatomic, retain) NSDate * readDate;
+@property (nonatomic, retain) NSNumber * readHour;
+@property (nonatomic, retain) NSNumber * recordID;
+@property (nonatomic, retain) Book *bookID;
+
+- (NSString *)readDateToString;
+@end
